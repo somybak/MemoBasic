50954a0b82384094a7e41d39ef5071d41bdb9437
javafa <nigne2016@gmail.com>
2017-02-14 16:49:11 +0900

    init

---
+++ app/src/main/java/com/veryworks/android/bbsbasic/ListAdapter.java
@@ -0,0 +1,39 @@
+package com.veryworks.android.bbsbasic;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
+
+    public ListAdapter(Context context) {
+
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.layout_list_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(View view) {
+            super(view);
+
+        }
+    }
+}
